39b9dbc372652fd2e052326136d055ed9a5811c2
DanushkaNan <danushkanan@lolctech.com>
2025-03-21 15:53:58 +0530

    Added new APIs to the Backend.

--- src/controller/examController.js
+++ src/controller/examController.js
@@ -1,18 +1,21 @@
+require('dotenv').config();
+const { google } = require("googleapis");
+const fs = require("fs");
+const path = require("path");
 const Department = require("../models/department");
 const Course = require("../models/course");
 const Exam = require("../models/exam");
 const BuyExam = require("../models/buyExam");
 const Student = require("../models/student");
 
-exports.getAllDepartments = async (req, res) => {
-    const departments = await Department.find();
-    res.json(departments);
-};
+const googleCredentials = JSON.parse(Buffer.from(process.env.GOOGLE_CREDENTIALS, 'base64').toString('utf-8'));
 
-exports.getAllCourses = async (req, res) => {
-    const courses = await Course.find();
-    res.json(courses);
-};
+const auth = new google.auth.GoogleAuth({
+    credentials: googleCredentials,
+    scopes: ["https://www.googleapis.com/auth/drive.file"],
+});
+
+const drive = google.drive({ version: "v3", auth });
 
 exports.uploadExam = async (req, res) => {
     const { authorId, author, name, department, course, level, unitName, description, price, topics } = req.body;
@@ -22,8 +25,45 @@ exports.uploadExam = async (req, res) => {
     }
 
     const filePath = req.file.path;
+    const folderId = process.env.GOOGLE_DRIVE_FOLDER_ID;
 
     try {
+        const fileMetadata = {
+            name: req.file.originalname,
+            parents: [folderId],
+        };
+        const media = {
+            mimeType: req.file.mimetype,
+            body: fs.createReadStream(filePath),
+        };
+
+        const file = await drive.files.create({
+            resource: fileMetadata,
+            media: media,
+            fields: "id, webViewLink",
+        });
+
+        const googleDriveFileId = file.data.id;
+        const googleDriveFileUrl = file.data.webViewLink;
+
+        await drive.permissions.create({
+            fileId: googleDriveFileId,
+            requestBody: {
+                role: "reader",
+                type: "anyone",
+                allowFileDiscovery: false,
+            },
+        });
+
+        await drive.files.update({
+            fileId: googleDriveFileId,
+            requestBody: {
+                viewersCanCopyContent: false,
+                copyRequiresWriterPermission: true,
+                writersCanShare: false,
+            },
+        });
+
         const exam = new Exam({
             authorId,
             author,
@@ -33,15 +73,17 @@ exports.uploadExam = async (req, res) => {
             level,
             unitName,
             description,
-            filePath,
+            filePath: googleDriveFileUrl,
             price,
             topics: JSON.parse(topics),
         });
 
         await exam.save();
-        res.status(201).json({ message: "Exam uploaded successfully" });
+        res.status(201).json({ message: "Exam uploaded successfully", filePath: googleDriveFileUrl });
     } catch (error) {
         res.status(500).json({ error: error.message });
+    } finally {
+        fs.unlinkSync(filePath);
     }
 };
 
